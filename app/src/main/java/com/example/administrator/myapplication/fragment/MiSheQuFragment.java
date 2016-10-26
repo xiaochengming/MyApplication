9c58dfd054ddedc3289360eda4361b8688123349
xiaochengming <xiaochengming@qq.com>
2016-10-26 21:34:10 +0800

    肖承明 提交 2

--- app/src/main/java/com/example/administrator/myapplication/fragment/MiSheQuFragment.java
+++ app/src/main/java/com/example/administrator/myapplication/fragment/MiSheQuFragment.java
@@ -164,11 +164,11 @@ public class MiSheQuFragment extends Fragment {
                                 @Override
                                 public void onClick(View v) {
                                     Log.i("MiSheQuFragment", "onClick:布局点击 ");
-                                    Post post1 = (Post) v.getTag();
+
                                     //跳到贴子详情
 
                                     Intent intent = new Intent(getActivity(), DongtaiActivity.class);
-                                    intent.putExtra("post", post1);
+                                    intent.putExtra("post", list.get(position));
                                     startActivity(intent);
                                 }
                             });
